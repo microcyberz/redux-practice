6c228b1315eb09985b8d3a9c93e0dc70fdfbc46c
microcyberz <ali333291@gmail.com>
2018-04-18 22:47:41 +0500

    remove react build-in state, use redux. Add code to postActions, it will use thunk for asynchronous request, then handle it in postReducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,13 +1,13 @@
 import { FETCH_POSTS, NEW_POST } from '../actions/types';
 
 const initialState = {
-    items: [], //represents post coming from action, we put fetch request in action
-    item: {} //represents the single post that we add and then we get response back
+  items: [], //represents post coming from action, we put fetch request in action
+  item: {} //represents the single post that we add and then we get response back
 }
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
