80bc384112d2982c86da67411596fd27f651648a
microcyberz <ali333291@gmail.com>
2018-04-18 17:54:01 +0500

    Import Provider and wrap App insite the Provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 import Posts from './components/Posts';
 import Postform from './components/Postform'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Postform />
-        <hr />
-        <Posts />
-      </div>
+      <Provider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
