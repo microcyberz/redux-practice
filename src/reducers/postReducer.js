d707f49d63070943a03cad80dac6c6233cb14ee8
microcyberz <ali333291@gmail.com>
2018-04-18 22:22:29 +0500

    write code of postReducer [add initialstate and then export reducer function having switch]

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,1 +1,13 @@
-import { FETCH_POSTS, NEW_POST } from '../actions/types'
+import { FETCH_POSTS, NEW_POST } from '../actions/types';
+
+const initialState = {
+    items: [], //represents post coming from action, we put fetch request in action
+    item: {} //represents the single post that we add and then we get response back
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
