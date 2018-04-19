355a74bb0dab95d1f05e08f78bff7ca06babbfaf
microcyberz <ali333291@gmail.com>
2018-04-20 00:35:55 +0500

    Add createPost action and implement it, connect Postform component to redux

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -21,3 +21,20 @@ export const fetchPosts = () => dispatch => {
       })
     );
 };
+
+export const createPost = (post) => dispatch => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  })
+    .then(res => res.json())
+    .then(post =>
+      dispatch({
+        type: NEW_POST,
+        payload: post
+      })
+    );
+};
