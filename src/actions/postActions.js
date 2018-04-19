72330a2667d376356427204a4009d7dc208c4b89
microcyberz <ali333291@gmail.com>
2018-04-19 08:42:55 +0500

    Connect Post component with redux store

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -14,8 +14,10 @@ import { FETCH_POSTS, NEW_POST } from './types';
 export const fetchPosts = () => dispatch => {
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(posts => dispatch({
-      type: FETCH_POSTS,
-      poyload: posts
-    }));
-}
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
